372e75710d666d0974753d85ba8140c336164617
jfcamacho <jefferson-camacho@hotmail.com>
2024-11-19 02:23:39 -0500

    variables

--- app/config.py
+++ app/config.py
@@ -7,5 +7,10 @@ load_dotenv()
 
 SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
 DATABASE_URL = os.getenv("DATABASE_URL")
+print(f"DATABASE_URL: {SECRET_KEY}")
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+# Reemplazar el esquema incorrecto
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
--- app/database.py
+++ app/database.py
@@ -2,9 +2,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from .config import DATABASE_URL
+from app.config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
+
+print(f"DATABASE_URL: {DATABASE_URL}")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
