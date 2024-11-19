fe4b9a409dd7fc3a47cc073bb21a8ff4d35a9193
Jefferson Camacho <jefferson-camacho@hotmail.com>
2024-11-19 11:58:35 -0500

    Update users.py

--- app/routers/users.py
+++ app/routers/users.py
@@ -69,6 +69,7 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db:
         key="access_token",
         value=access_token,
         httponly=True,
-        secure=True,  # Usa HTTPS en producción
+        secure: True, // Requiere HTTPS
+        sameSite: 'None', // Permite solicitudes cross-site
     )
     return {"access_token": access_token, "token_type": "bearer", "user": user}
